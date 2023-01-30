71e0e8dc61618718c3330bee60e574e625df92d0
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-01-30 11:16:14 +0700

    change the skills content

--- src/App.tsx
+++ src/App.tsx
@@ -1,129 +1,133 @@
-import { Link, useLocation } from '@solidjs/router';
-import { Component, createSignal, For, onMount } from 'solid-js';
-import MyAudio from '@/assets/Serenity.mp3';
-import '@/App.css';
-import Router from './Router';
+import { Link, useLocation } from "@solidjs/router";
+import { Component, createSignal, For, onMount } from "solid-js";
+import MyAudio from "@/assets/Serenity.mp3";
+import "@/App.css";
+import Router from "./Router";
 
 const paths = [
-	{
-		title: 'Profile',
-		icon: 'mdi mdi-account-settings-outline',
-		path: '/',
-	},
-	{
-		title: 'Contact',
-		icon: 'mdi mdi-phone-outline',
-		path: '/contact',
-	},
-	{
-		title: 'Project',
-		icon: 'mdi mdi-book-outline',
-		path: '/project',
-	},
-	{
-		title: 'About',
-		icon: 'mdi mdi-view-gallery-outline',
-		path: '/about',
-	},
+  {
+    title: "Profile",
+    icon: "mdi mdi-account-settings-outline",
+    path: "/",
+  },
+  {
+    title: "Contact",
+    icon: "mdi mdi-phone-outline",
+    path: "/contact",
+  },
+  {
+    title: "Project",
+    icon: "mdi mdi-book-outline",
+    path: "/project",
+  },
+  {
+    title: "About",
+    icon: "mdi mdi-view-gallery-outline",
+    path: "/about",
+  },
 ];
 
 const App: Component = () => {
-	const location = useLocation();
-	const [audio, setAudio] = createSignal<HTMLAudioElement>(document.createElement('audio'));
-	const [audioPlaying, setAudioPlaying] = createSignal<boolean>(false);
-	const [darkMode, setDarkMode] = createSignal<boolean>(false);
+  const location = useLocation();
+  const [audio, setAudio] = createSignal<HTMLAudioElement>(
+    document.createElement("audio")
+  );
+  const [audioPlaying, setAudioPlaying] = createSignal<boolean>(false);
+  const [darkMode, setDarkMode] = createSignal<boolean>(false);
 
-	function handleMusic() {
-		if (audioPlaying()) {
-			audio().pause();
-		} else {
-			audio().play();
-		}
-		setAudioPlaying(!audioPlaying());
-	}
+  function handleMusic() {
+    if (audioPlaying()) {
+      audio().pause();
+    } else {
+      audio().play();
+    }
+    setAudioPlaying(!audioPlaying());
+  }
 
-	function darkInit() {
-		if (localStorage.getItem('dark')) {
-			setDarkMode(true);
-			document.documentElement.classList.add('dark');
-		} else {
-			setDarkMode(false);
-			document.documentElement.classList.remove('dark');
-		}
-	}
+  function darkInit() {
+    if (localStorage.getItem("dark")) {
+      setDarkMode(true);
+      document.documentElement.classList.add("dark");
+    } else {
+      setDarkMode(false);
+      document.documentElement.classList.remove("dark");
+    }
+  }
 
-	function toggleDark() {
-		setDarkMode(!darkMode());
-		if (darkMode()) {
-			localStorage.setItem('dark', '1');
-		} else {
-			localStorage.removeItem('dark');
-		}
-		darkInit();
-	}
+  function toggleDark() {
+    setDarkMode(!darkMode());
+    if (darkMode()) {
+      localStorage.setItem("dark", "1");
+    } else {
+      localStorage.removeItem("dark");
+    }
+    darkInit();
+  }
 
-	onMount(() => {
-		const music = document.createElement('audio');
-		music.src = MyAudio;
-		music.loop = true;
+  onMount(() => {
+    const music = document.createElement("audio");
+    music.src = MyAudio;
+    music.loop = true;
 
-		setAudio(music);
-		darkInit();
-	});
+    setAudio(music);
+    darkInit();
+  });
 
-	return (
-		<div>
-			<div class="min-h-screen bg-gray-50 text-black dark:(bg-gray-800 text-white)">
-				<div class="nav h-15 px-6 flex z-20 flex-row items-center bg-white dark:bg-gray-900 shadow lg:(fixed left-0 bottom-0 top-0 w-18 h-screen flex-col py-10 justify-between) transition relative">
-					<div class="flex-1">
-						<Link
-							href="/"
-							class="text-blue-500 text-2xl text-shadow transform lg:(-rotate-90 inline-block mt-8)"
-						>
-							Alfiansyah
-						</Link>
-					</div>
-					<div class="fixed z-20 bottom-5 left-5 right-5 rounded-full transition bg-white dark:bg-gray-900 shadow p-2 flex flex-row justify-evenly px-4 lg:(flex-1 flex-col relative p-0 left-0 right-0 py-4 mb-20 shadow-none)">
-						<For each={paths}>
-							{(item) => (
-								<Link
-									href={item.path}
-									title={item.title}
-									class="px-3 text-gray-600 hover:text-blue-500 transform transition hover:-translate-y-1 (lg:px-0 py-3)"
-									classList={{ '!text-blue-500': location.pathname == item.path }}
-								>
-									<i class={'mdi text-2xl ' + item.icon}></i>
-								</Link>
-							)}
-						</For>
-					</div>
-					<div class="flex-1 text-right lg:(absolute bottom-5 left-0 right-0 flex items-center justify-center)">
-						<button class="outline-none" onClick={toggleDark}>
-							<i
-								class="text-xl mdi text-yellow-500 mdi-weather-night"
-								classList={{ 'mdi-weather-sunny': !darkMode() }}
-							></i>
-						</button>
-					</div>
-				</div>
-				<div class="px-4 pb-16 py-10 lg:(ml-18 py-0 pb-0) lg:min-h-screen view">
-					<Router></Router>
-				</div>
-				<button
-					class="outline-none fixed lg:top-10 top-20 right-5 w-10 z-10 h-10 flex items-center justify-center rounded-full transition bg-purple-500 shadow-lg"
-					onClick={handleMusic}
-				>
-					<i
-						class="mdi dark:text-gray-900 text-white mdi-access-point text-xl"
-						classList={{ 'animate-spin': audioPlaying() }}
-					></i>
-				</button>
-			</div>
-			<div class="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center font-semibold lg:text-5xl text-3xl bg-white dark:bg-gray-900 z-30 text-blue-500 animate-loading pointer-events-none">
-				<span class="animate-bounce">Loading...</span>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div class="min-h-screen bg-gray-50 text-black dark:(bg-gray-800 text-white)">
+        <div class="nav h-15 px-6 flex z-20 flex-row items-center bg-white dark:bg-gray-900 shadow lg:(fixed left-0 bottom-0 top-0 w-18 h-screen flex-col py-10 justify-between) transition relative">
+          <div class="flex-1">
+            <Link
+              href="/"
+              class="text-blue-500 text-2xl text-shadow transform lg:(-rotate-90 inline-block mt-8)"
+            >
+              Alfiansyah
+            </Link>
+          </div>
+          <div class="fixed z-20 bottom-5 left-5 right-5 rounded-full transition bg-white dark:bg-gray-900 shadow p-2 flex flex-row justify-evenly px-4 lg:(flex-1 flex-col relative p-0 left-0 right-0 py-4 mb-20 shadow-none)">
+            <For each={paths}>
+              {(item) => (
+                <Link
+                  href={item.path}
+                  title={item.title}
+                  class="px-3 text-gray-600 hover:text-blue-500 transform transition hover:-translate-y-1 (lg:px-0 py-3)"
+                  classList={{
+                    "!text-blue-500": location.pathname == item.path,
+                  }}
+                >
+                  <i class={"mdi text-2xl " + item.icon}></i>
+                </Link>
+              )}
+            </For>
+          </div>
+          <div class="flex-1 text-right lg:(absolute bottom-5 left-0 right-0 flex items-center justify-center)">
+            <button class="outline-none" onClick={toggleDark}>
+              <i
+                class="text-xl mdi text-yellow-500 mdi-weather-night"
+                classList={{ "mdi-weather-sunny": !darkMode() }}
+              ></i>
+            </button>
+          </div>
+        </div>
+        <div class="px-4 pb-16 py-10 lg:(ml-18 py-0 pb-0) lg:min-h-screen view">
+          <Router></Router>
+        </div>
+        <button
+          class="outline-none fixed lg:top-10 top-20 right-5 w-10 z-10 h-10 flex items-center justify-center rounded-full transition bg-purple-500 shadow-lg"
+          onClick={handleMusic}
+        >
+          <i
+            class="mdi dark:text-gray-900 text-white mdi-access-point text-xl"
+            classList={{ "animate-spin": audioPlaying() }}
+          ></i>
+        </button>
+      </div>
+      <div class="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center font-semibold lg:text-5xl text-3xl bg-white dark:bg-gray-900 z-30 text-blue-500 animate-loading pointer-events-none">
+        <span class="animate-bounce">Loading...</span>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,75 +1,135 @@
-import { Link } from '@solidjs/router';
-import { Component, For } from 'solid-js';
+import { Link } from "@solidjs/router";
+import { Component, For } from "solid-js";
 
 const About: Component = () => {
-	return (
-		<>
-			<div class="lg:min-h-100vh flex flex-wrap -mx-4 lg:py-10 justify-center">
-				<div class="w-full text-center mb-5 mt-5 px-4">
-					<div class="text-blue-500 font-bold text-3xl">ABOUT ME</div>
-					<div>A short piece about my personal background</div>
-				</div>
-				<div class="w-full lg:w-4/6 p-4">
-					<div class="bg-white dark:bg-gray-900 rounded shadow p-4 mb-4 transition hover:(transform scale-105)">
-						<p class="mb-4">
-							Hello! My Name is Muhammad Izza Alfiansyah and you can call me Alfian. I Live in
-							Jember Regency, East Java, Indonesia. I started programming when I was in high school
-							at{' '}
-							<a href="https://esekmaprima.com" target="_blank" class="text-purple-500">
-								SMK PGRI 5 Jember
-							</a>
-							, The most meritorious place in my ability development.
-						</p>
-						<p>
-							I am very enthusiastic about learning new things in the world of technology. Now, I
-							work as a fullstack developer at{' '}
-							<a href="https://fopegram.id" target="_blank" class="text-purple-500">
-								Fopegram
-							</a>
-							. I am also a student who is studying at{' '}
-							<a href="https://polije.ac.id" target="_blank" class="text-purple-500">
-								Politeknik Negeri Jember
-							</a>
-							.
-						</p>
-					</div>
-					<div class="p-4 shadow bg-white dark:bg-gray-900 rounded mb-4 transition hover:(transform scale-105)">
-						Some of my skills are:
-						<div class="grid lg:grid-cols-3 md:grid-cols-2 mt-3 mb-3">
-							<For
-								each={[
-									'HTML',
-									'CSS',
-									'JS',
-									'PHP',
-									'DART',
-									'CodeIgniter',
-									'Laravel',
-									'Bootstrap',
-									'Tailwind',
-									'React',
-									'Vue',
-									'Svelte',
-									'Flutter',
-								]}
-							>
-								{(item) => (
-									<div>
-										<li>{item}</li>
-									</div>
-								)}
-							</For>
-						</div>
-						You can see my experience at{' '}
-						<Link href="/project" class="text-purple-500">
-							project
-						</Link>
-						.
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  const skills = [
+    {
+      icon: "https://cdn-icons-png.flaticon.com/512/1051/1051277.png",
+      title: "HTML",
+    },
+    {
+      icon: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+      title: "CSS",
+    },
+    {
+      icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+      title: "Javascript",
+    },
+    {
+      icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2560px-PHP-logo.svg.png",
+      title: "PHP",
+    },
+    {
+      icon: "https://cdn-icons-png.flaticon.com/512/919/919852.png",
+      title: "Python",
+    },
+    {
+      icon: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Dart-logo.png",
+      title: "Dart",
+    },
+    {
+      icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1280px-Bootstrap_logo.svg.png",
+      title: "Bootstrap",
+    },
+    {
+      icon: "https://raw.githubusercontent.com/gilbarbara/logos/master/logos/codeigniter-icon.svg",
+      title: "Codeigniter",
+    },
+    {
+      icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/985px-Laravel.svg.png",
+      title: "Laravel",
+    },
+    {
+      icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+      title: "React",
+    },
+    {
+      icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png",
+      title: "Vue",
+    },
+    {
+      icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/1200px-Svelte_Logo.svg.png",
+      title: "Svelte",
+    },
+    {
+      icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2048px-Tailwind_CSS_Logo.svg.png",
+      title: "Tailwind",
+    },
+    {
+      icon: "https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png",
+      title: "NodeJS",
+    },
+    {
+      icon: "https://www.solidjs.com/img/logo/without-wordmark/logo.png",
+      title: "Solid",
+    },
+    {
+      icon: "https://assets.website-files.com/60d251a34163cf29e1220806/626accd8eefaec54f23310ba_flutter%20developer%20logo.png",
+      title: "Flutter",
+    },
+  ];
+
+  return (
+    <>
+      <div class="lg:min-h-100vh flex flex-wrap -mx-4 lg:py-10 justify-center">
+        <div class="w-full text-center mb-5 mt-5 px-4">
+          <div class="text-blue-500 font-bold text-3xl">ABOUT ME</div>
+          <div>A short piece about my personal background</div>
+        </div>
+        <div class="w-full lg:w-4/6 p-4">
+          <div class="bg-white dark:bg-gray-900 rounded shadow p-4 mb-4 transition hover:(transform scale-105)">
+            <p class="mb-4">
+              Hello! My Name is Muhammad Izza Alfiansyah and you can call me
+              Alfian. I Live in Jember Regency, East Java, Indonesia. I started
+              programming when I was in high school at{" "}
+              <a
+                href="https://esekmaprima.com"
+                target="_blank"
+                class="text-purple-500"
+              >
+                SMK PGRI 5 Jember
+              </a>
+              , The most meritorious place in my ability development.
+            </p>
+            <p>
+              I am very enthusiastic about learning new things in the world of
+              technology. Now, I am a student who is studying at{" "}
+              <a
+                href="https://polije.ac.id"
+                target="_blank"
+                class="text-purple-500"
+              >
+                Politeknik Negeri Jember
+              </a>
+              .
+            </p>
+          </div>
+          <div class="p-4 shadow bg-white dark:bg-gray-900 rounded mb-4 transition hover:(transform scale-105)">
+            Some of my skills are:
+            <div class="grid lg:grid-cols-8 grid-cols-4 my-10 lg:gap-8 gap-4 mx-5 justify-center items-center">
+              <For each={skills}>
+                {(item) => (
+                  <div class="flex flex-col items-center justify-end h-20">
+                    <img
+                      src={item.icon}
+                      alt={item.title}
+                      class="lg:h-16 h-11 mb-1"
+                    />
+                    <div class="text-center text-sm">{item.title}</div>
+                  </div>
+                )}
+              </For>
+            </div>
+            You can see my experience at{" "}
+            <Link href="/project" class="text-purple-500">
+              project
+            </Link>
+            .
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
