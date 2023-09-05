a6d3fc5ccb44e6e23a84cd33d4f6f19b97d9af22
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-09-06 05:21:43 +0700

    mv the career data to folder

---
+++ src/datas/career.ts
@@ -0,0 +1,38 @@
+interface Career {
+  company: string;
+  position: string;
+  date: string;
+  image: string;
+}
+
+const careers: Career[] = [
+  {
+    company: "Mascitra.com",
+    position: "Leader Junior Full Stack Developer",
+    date: "Dec 2019 - Apr 2020",
+    image:
+      "https://media.licdn.com/dms/image/C560BAQEm7dlvSIrp6Q/company-logo_200_200/0/1622715903607?e=2147483647&v=beta&t=5Y064WlK7ncmZNqF3O6Uno2rAklIz1QnVHVRw6maZPU",
+  },
+  {
+    company: "Fopegram",
+    position: "Back End Developer",
+    date: "Oct 2020 - Agu 2022",
+    image:
+      "https://png.pngtree.com/png-clipart/20221110/original/pngtree-f-logo-business-symbols-vector-template-letter-picture-image_3626283.png",
+  },
+  {
+    company: "Bima Technologies",
+    position: "Junior IoT Engineer",
+    date: "Sept 2023 - Now",
+    image:
+      "https://img.mbizmarket.co.id/company/thumbs/343x343/2022/10/25/d2e17cc3d997e596ce8c0c035029920b.jpg",
+  },
+  {
+    company: "Freelance",
+    position: "Full Stack Developer",
+    date: "Mar 2022 - Now",
+    image: "https://miro.medium.com/max/1200/1*BBct14l3of-42X9TjH65QA.png",
+  },
+];
+
+export default careers;
--- src/views/Career.tsx
+++ src/views/Career.tsx
@@ -1,48 +1,14 @@
 import Title from "@/components/title/title";
+import careers from "@/datas/career";
 import { Component } from "solid-js";
 
 const Career: Component = () => {
-  const data: Array<{
-    company: string;
-    position: string;
-    date: string;
-    image: string;
-  }> = [
-    {
-      company: "Mascitra.com",
-      position: "Leader Junior Full Stack Developer",
-      date: "Dec 2019 - Apr 2020",
-      image:
-        "https://media.licdn.com/dms/image/C560BAQEm7dlvSIrp6Q/company-logo_200_200/0/1622715903607?e=2147483647&v=beta&t=5Y064WlK7ncmZNqF3O6Uno2rAklIz1QnVHVRw6maZPU",
-    },
-    {
-      company: "Fopegram",
-      position: "Back End Developer",
-      date: "Oct 2020 - Agu 2022",
-      image:
-        "https://png.pngtree.com/png-clipart/20221110/original/pngtree-f-logo-business-symbols-vector-template-letter-picture-image_3626283.png",
-    },
-    {
-      company: "Bima Technologies",
-      position: "Junior IoT Engineer",
-      date: "Sept 2023 - Now",
-      image:
-        "https://img.mbizmarket.co.id/company/thumbs/343x343/2022/10/25/d2e17cc3d997e596ce8c0c035029920b.jpg",
-    },
-    {
-      company: "Freelance",
-      position: "Full Stack Developer",
-      date: "Mar 2022 - Now",
-      image: "https://miro.medium.com/max/1200/1*BBct14l3of-42X9TjH65QA.png",
-    },
-  ];
-
   return (
     <>
       <div class="w-full">
         <Title title="Career" subtitle="The amazing of my experience" />
         <div class="w-full">
-          {data.map((item) => (
+          {careers.map((item) => (
             <div class="bg-white !bg-opacity-30 dark:bg-gray-900 rounded-lg shadow p-3 mb-4 transition transform hover:scale-[103%] mb-3">
               <div class="flex md:flex-row flex-col justify-between items-center">
                 <div class="md:w-auto w-full">
