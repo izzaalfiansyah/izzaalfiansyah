0fd246997067203e170c2088892ef5bc093caff0
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2024-09-11 11:33:19 +0700

    update cv

--- src/datas/career.ts
+++ src/datas/career.ts
@@ -7,31 +7,25 @@ interface Career {
 
 const careers: Career[] = [
   {
-    company: "Mascitra.com",
-    position: "Leader Junior Full Stack Developer",
-    date: "Dec 2019 - Apr 2020",
+    company: "PT. Mascitra Teknologi Informasi",
+    position: "Leader Junior Full Stack Web Developer",
+    date: "2019 - 2020",
     image:
       "https://media.licdn.com/dms/image/C560BAQEm7dlvSIrp6Q/company-logo_200_200/0/1622715903607?e=2147483647&v=beta&t=5Y064WlK7ncmZNqF3O6Uno2rAklIz1QnVHVRw6maZPU",
   },
   {
-    company: "Fopegram",
-    position: "Back End Developer",
-    date: "Oct 2020 - Agu 2022",
+    company: "CV. Fopegram",
+    position: "Full Stack Developer",
+    date: "2020 - 2021",
     image:
       "https://png.pngtree.com/png-clipart/20221110/original/pngtree-f-logo-business-symbols-vector-template-letter-picture-image_3626283.png",
   },
   {
-    company: "Bima Technologies",
-    position: "Junior IoT Engineer",
-    date: "Sept 2023 - Now",
+    company: "CV. Bima Technologies",
+    position: "Junior IoT Engineer Internship",
+    date: "2023",
     image:
       "https://img.mbizmarket.co.id/company/thumbs/343x343/2022/10/25/d2e17cc3d997e596ce8c0c035029920b.jpg",
-  },
-  {
-    company: "Freelance",
-    position: "Full Stack Developer",
-    date: "Mar 2022 - Now",
-    image: "https://miro.medium.com/max/1200/1*BBct14l3of-42X9TjH65QA.png",
   },
 ];
 
