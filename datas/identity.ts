817ea0266a6abaaa49aa06577bc9fc33571256fa
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2026-01-11 01:05:35 +0700

    update: cv

--- datas/identity.ts
+++ datas/identity.ts
@@ -1,23 +1,25 @@
 import { careers } from "./career";
 
 export const profile = {
-  firstName: "Muhammad",
-  lastName: "Izza Alfiansyah",
-  position: "Full Stack & Mobile Flutter Developer",
-  email: "iansyah724@gmail.com",
-  phone: "+6281231921351",
-  address: "Jember, East Java, Indonesia",
-  get name(): string {
-    return this.firstName + " " + this.lastName;
-  },
-  get siteName(): string {
-    return this.name + " | " + this.position;
-  },
-  get description(): string {
-    return `Hello! My Name is ${this.name} and you can call me Alfian. I am a ${
-      this.position
-    } at ${
-      careers[careers.length - 1].company
-    } I Live in Jember Regency, East Java Indonesia. I started programming when I was in high school at SMK PGRI 5 Jember, The most meritorious place in my ability development. I am very enthusiastic about learning new things in the world of technology.`;
-  },
+	firstName: "Muhammad",
+	lastName: "Izza Alfiansyah",
+	position: "Full Stack & Mobile Flutter Developer",
+	email: "iansyah724@gmail.com",
+	phone: "+6281231921351",
+	address: "Jember, East Java, Indonesia",
+	get name(): string {
+		return this.firstName + " " + this.lastName;
+	},
+	get siteName(): string {
+		return this.name + " | " + this.position;
+	},
+	get description(): string {
+		return `Hello! My Name is ${this.name} and you can call me Alfian. I am a ${
+			this.position
+		} at ${
+			careers[careers.length - 1].company
+		} I Live in Jember Regency, East Java Indonesia. I started programming when I was in high school at SMK PGRI 5 Jember, The most meritorious place in my ability development. I am very enthusiastic about learning new things in the world of technology.`;
+	},
+	cv_url:
+		"https://drive.google.com/file/d/1rMLo0nFkvUWE1IXdSl44F83oR2u3BUur/view?usp=sharing",
 };
--- pages/index.vue
+++ pages/index.vue
@@ -6,17 +6,13 @@ import { projects } from "~/datas/project";
 <template>
   <SeoMeta></SeoMeta>
   <div
-    class="rounded-lg min-h-[80vh] bg-gradient-to-t from-primary/15 to-transparent flex flex-wrap items-center justify-between overflow-hidden relative"
-  >
-    <img
-      src="/favicon.ico"
+    class="rounded-lg min-h-[80vh] bg-gradient-to-t from-primary/15 to-transparent flex flex-wrap items-center justify-between overflow-hidden relative">
+    <img src="/favicon.ico"
       class="absolute h-[50vh] md:scale-75 scale-50 top-24 md:right-0 right-auto left-auto object-cover md:inline-block hidden"
-      data-aos="fade-left"
-    />
+      data-aos="fade-left" />
     <div
       class="w-full space-y-10 relative backdrop-blur-xs lg:h-auto min-h-[80vh] flex flex-col justify-center md:px-10 px-5"
-      data-aos="fade-right"
-    >
+      data-aos="fade-right">
       <div class="lg:text-6xl text-5xl font-bold">
         {{ profile.firstName }} <br />
         <span class="text-primary">{{ profile.lastName }}</span>
@@ -25,18 +21,10 @@ import { projects } from "~/datas/project";
         <span class="font-thin text-xl">{{ profile.position }}</span>
       </div>
       <div class="flex items-center space-x-6 flex-wrap">
-        <a
-          href="/Muhammad Izza Alfiansyah - CV.pdf"
-          target="_blank"
-          class="px-8 py-3 rounded bg-primary text-white outline-none mb-3"
-          download
-        >
+        <a :href="profile.cv_url" target="_blank" class="px-8 py-3 rounded bg-primary text-white outline-none mb-3">
           Download CV
         </a>
-        <NuxtLink
-          href="/contact"
-          class="px-8 py-3 rounded dark:bg-gray-900 bg-white outline-none mb-3"
-        >
+        <NuxtLink href="/contact" class="px-8 py-3 rounded dark:bg-gray-900 bg-white outline-none mb-3">
           Contact Me
         </NuxtLink>
       </div>
@@ -47,17 +35,12 @@ import { projects } from "~/datas/project";
     <div class="mt-10">
       <div class="grid lg:grid-cols-2 grid-cols-1 gap-8">
         <template v-for="(project, i) in projects.slice(0, 4)">
-          <ProjectItem
-            :data-aos="i % 2 === 0 ? 'fade-right' : 'fade-left'"
-            :project="project"
-          />
+          <ProjectItem :data-aos="i % 2 === 0 ? 'fade-right' : 'fade-left'" :project="project" />
         </template>
       </div>
     </div>
     <div class="mt-10 text-center" data-aos="fade-up">
-      <NuxtLink href="/project" class="bg-primary text-white rounded px-8 py-3"
-        >Show More</NuxtLink
-      >
+      <NuxtLink href="/project" class="bg-primary text-white rounded px-8 py-3">Show More</NuxtLink>
     </div>
   </div>
   <InterestTogether></InterestTogether>
