0f0c30c970bd3231c61ff3a5b1ad15f289ef8dab
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2026-06-06 10:04:44 +0700

    refactor(contact): make constant from contact url

---
+++ src/data/constant.ts
@@ -0,0 +1,8 @@
+export const GITHUB_URL = "https://github.com/izzaalfiansyah"
+export const LINKEDIN_URL =
+  "https://www.linkedin.com/in/muhammad-izza-alfiansyah-76a473220"
+export const WHATSAPP_URL = "https://api.whatsapp.com/send?phone=6281231921351"
+export const EMAIL_URL = "mailto:iansyah724@gmail.com"
+export const INSTAGRAM_URL = "https://www.instagram.com/izzaalfiansyah_"
+export const FACEBOOK_URL =
+  "https://facebook.com/profile.php?id=100027236859562"
--- src/data/contact.ts
+++ src/data/contact.ts
@@ -5,6 +5,14 @@ import Instagram from "@/components/icon/Instagram.astro"
 import Linkedin from "@/components/icon/Linkedin.astro"
 import type { AIconProps } from "@/components/icon/type"
 import Whatsapp from "@/components/icon/Whatsapp.astro"
+import {
+  EMAIL_URL,
+  FACEBOOK_URL,
+  GITHUB_URL,
+  INSTAGRAM_URL,
+  LINKEDIN_URL,
+  WHATSAPP_URL,
+} from "./constant"
 
 export interface ContactLink {
   name: string
@@ -17,36 +25,36 @@ export const contactLinks: ContactLink[] = [
   {
     name: "Github",
     icon: Github,
-    url: "https://github.com/izzaalfiansyah",
+    url: GITHUB_URL,
   },
   {
     name: "LinkedIn",
     icon: Linkedin,
-    url: "https://www.linkedin.com/in/muhammad-izza-alfiansyah-76a473220",
+    url: LINKEDIN_URL,
     color: "#0077B5",
   },
   {
     name: "Whatsapp",
     icon: Whatsapp,
-    url: "https://api.whatsapp.com/send?phone=6281231921351",
+    url: WHATSAPP_URL,
     color: "#25D366",
   },
   {
     name: "Email",
     icon: Email,
-    url: "mailto:iansyah724@gmail.com",
+    url: EMAIL_URL,
     color: "#EA4335",
   },
   {
     name: "Instagram",
     icon: Instagram,
-    url: "https://www.instagram.com/izzaalfiansyah_/",
+    url: INSTAGRAM_URL,
     color: "#E1306C",
   },
   {
     name: "Facebook",
     icon: Facebook,
-    url: "https://facebook.com/profile.php?id=100027236859562",
+    url: FACEBOOK_URL,
     color: "#1877F2",
   },
 ]
