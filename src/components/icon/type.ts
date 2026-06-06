9240dffbb00cfd5345aeb30b7cbe4a4991f1b3e0
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2026-06-06 09:54:17 +0700

    chore(contact): add related contacts icon

---
+++ src/components/icon/type.ts
@@ -0,0 +1,5 @@
+export interface AIconProps {
+  height?: number | string
+  width?: number | string
+  class?: string
+}
--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,44 +1,52 @@
+import Email from "@/components/icon/Email.astro"
+import Facebook from "@/components/icon/Facebook.astro"
+import Github from "@/components/icon/Github.astro"
+import Instagram from "@/components/icon/Instagram.astro"
+import Linkedin from "@/components/icon/Linkedin.astro"
+import type { AIconProps } from "@/components/icon/type"
+import Whatsapp from "@/components/icon/Whatsapp.astro"
+
 export interface ContactLink {
-	name: string;
-	icon: string;
-	url: string;
-	color?: string;
+  name: string
+  icon: (_props: AIconProps) => unknown
+  url: string
+  color?: string
 }
 
 export const contactLinks: ContactLink[] = [
-	{
-		name: "Github",
-		icon: "https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg",
-		url: "https://github.com/izzaalfiansyah",
-	},
-	{
-		name: "LinkedIn",
-		icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/1200px-LinkedIn_icon_circle.svg.png",
-		url: "https://www.linkedin.com/in/muhammad-izza-alfiansyah-76a473220",
-		color: "#0077B5",
-	},
-	{
-		name: "Whatsapp",
-		icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/767px-WhatsApp.svg.png",
-		url: "https://api.whatsapp.com/send?phone=6281231921351",
-		color: "#25D366",
-	},
-	{
-		name: "Email",
-		icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png",
-		url: "mailto:iansyah724@gmail.com",
-		color: "#EA4335",
-	},
-	{
-		name: "Instagram",
-		icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png",
-		url: "https://www.instagram.com/izzaalfiansyah_/",
-		color: "#E1306C",
-	},
-	{
-		name: "Facebook",
-		icon: "https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/640px-Facebook_f_logo_%282021%29.svg.png",
-		url: "https://facebook.com/profile.php?id=100027236859562",
-		color: "#1877F2",
-	},
-];
+  {
+    name: "Github",
+    icon: Github,
+    url: "https://github.com/izzaalfiansyah",
+  },
+  {
+    name: "LinkedIn",
+    icon: Linkedin,
+    url: "https://www.linkedin.com/in/muhammad-izza-alfiansyah-76a473220",
+    color: "#0077B5",
+  },
+  {
+    name: "Whatsapp",
+    icon: Whatsapp,
+    url: "https://api.whatsapp.com/send?phone=6281231921351",
+    color: "#25D366",
+  },
+  {
+    name: "Email",
+    icon: Email,
+    url: "mailto:iansyah724@gmail.com",
+    color: "#EA4335",
+  },
+  {
+    name: "Instagram",
+    icon: Instagram,
+    url: "https://www.instagram.com/izzaalfiansyah_/",
+    color: "#E1306C",
+  },
+  {
+    name: "Facebook",
+    icon: Facebook,
+    url: "https://facebook.com/profile.php?id=100027236859562",
+    color: "#1877F2",
+  },
+]
