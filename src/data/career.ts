8e0c02c1c08848577ae5d1c064d4f9ddab11c250
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2026-06-06 10:08:03 +0700

    chore(career): update career datas

--- src/data/career.ts
+++ src/data/career.ts
@@ -23,18 +23,18 @@ export const careers: Career[] = [
   {
     company: "CV. Bima Technologies",
     logo: "https://img.mbizmarket.co.id/company/thumbs/343x343/2022/10/25/d2e17cc3d997e596ce8c0c035029920b.jpg",
-    role: "Junior IoT Engineer Internship",
+    role: "Junior Network Engineer Internship",
     period: "Sep 2023 - Feb 2024",
   },
-  {
-    company: "CV. Fopegram",
-    logo: "https://png.pngtree.com/png-clipart/20221110/original/pngtree-f-logo-business-symbols-vector-template-letter-picture-image_3626283.png",
-    role: "Full Stack Developer",
-    period: "Oct 2020 - Aug 2021",
-  },
+  // {
+  //   company: "CV. Fopegram",
+  //   logo: "https://png.pngtree.com/png-clipart/20221110/original/pngtree-f-logo-business-symbols-vector-template-letter-picture-image_3626283.png",
+  //   role: "Full Stack Developer",
+  //   period: "Oct 2020 - Aug 2021",
+  // },
   {
     company: "PT. Mascitra Teknologi Informasi",
-    logo: "https://media.licdn.com/dms/image/C560BAQEm7dlvSIrp6Q/company-logo_200_200/0/1622715903607?e=2147483647&v=beta&t=5Y064WlK7ncmZNqF3O6Uno2rAklIz1QnVHVRw6maZPU",
+    logo: "https://mascitragroup.com/storage/brands/S0zgOcXaAg0gn3p0252aOOuIo96578f8JsaAPChU.png",
     role: "Leader Junior Full Stack Web Developer",
     period: "Dec 2019 - May 2020",
   },
